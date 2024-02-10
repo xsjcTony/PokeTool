f6976de2db4b8bee9b399f7348703fa3431668e9
Aelita <xsjctony@126.com>
2024-02-11 00:50:34 +1100

    feat(type-chart): fetch all type charts

--- src/app/type-chart/page.tsx
+++ src/app/type-chart/page.tsx
@@ -1,9 +1,22 @@
 import type { JSX } from 'react'
 
 
+type AllTypes = {
+  name: string
+  url: string
+}[]
+
+
 const TypeChart = async (): Promise<JSX.Element> => {
+
+  const allTypes = (await fetch('https://pokeapi.co/api/v2/type')
+    .then(async res => await res.json()) as { results: AllTypes })
+    .results
+    .filter(type => type.name !== 'shadow' && type.name !== 'unknown')
+
+
   return (
-    <div>123</div>
+    <pre>{JSON.stringify(allTypes, null, 2)}</pre>
   )
 }
 
